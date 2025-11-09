595052ada2464f76f83c0ab7742a79f4756644fe
laviniapark <laviniasoo03@gmail.com>
2025-11-09 16:08:15 -0300

    script para deploy adicionado

---
+++ script/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+RG="rg-s4-motuswatch"
+
+az group delete -g $RG --yes --no-wait
---
+++ script/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+LOCATION="brazilsouth"
+RG="rg-s4-motuswatch"
+ACR_NAME="motuswatchs4acr"
+APP_SERVICE_PLAN="plan-s4-motuswatch"
+WEBAPP_NAME="motuswatchs4webapp"
+USERNAME="RM559123"
+PASSWORD="fiap"
+
+git clone https://github.com/ucarols/Sprint4_java.git
+
+mv Sprint4_java/script/setup.sh .
+
+chmod +x setup.sh
+
+az group create --name $RG --location $LOCATION
+
+az appservice plan create \
+  --name $APP_SERVICE_PLAN \
+  --resource-group $RG \
+  --location $LOCATION \
+  --sku F1 \
+  --is-linux
+
+az webapp create \
+  --name $WEBAPP_NAME \
+  --resource-group $RG \
+  --plan $APP_SERVICE_PLAN \
+  --runtime "JAVA:17-java17"
+
+az webapp config appsettings set \
+  --name "$WEBAPP_NAME" \
+  --resource-group "$RG" \
+  --settings \
+    WEBSITES_PORT=8080 \
+    ACTIVE_PROFILE=prod \
+    DB_URL="jdbc:oracle:thin:@oracle.fiap.com.br:1521/ORCL" \
+    DB_USERNAME=$USERNAME \
+    DB_PASSWORD="$PASSWORD"
+
+cd Sprint4_java/
+
+mvn clean package -DskipTests
+
+## RODAR MANUALMENTE
+
+#cd Sprint4_java/
+#
+#az webapp deploy \
+#-g rg-s4-motuswatch -n motuswatchs4webapp \
+#--src-path ./target/moto-api-0.0.1-SNAPSHOT.jar \
+#--type jar
+#
+#az webapp log tail -g rg-s4-motuswatch -n motuswatchs4webapp
